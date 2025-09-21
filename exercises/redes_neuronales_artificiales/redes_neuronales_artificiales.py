3feeddb918cb715d935e654edb42b92403514d6b
DiegoLLP123 <ingeniero.diegollp@gmail.com>
2025-09-20 19:41:40 -0600

    ejercicio 2

--- exercises/redes_neuronales_artificiales/redes_neuronales_artificiales.py
+++ exercises/redes_neuronales_artificiales/redes_neuronales_artificiales.py
@@ -4,7 +4,7 @@ import random
 import sys
 import os
 from logging import DEBUG, INFO, WARNING, ERROR
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(_file_), '..', '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 from py_utils.logger import set_logging, plog
 
 # Llamada a la funcion para configurar el logging
